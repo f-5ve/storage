3a6e8178b76657241e1a9bdbeda95ef40c2070f2
F5ve <fivedeisgns@gmail.com>
2025-08-29 21:06:48 +0100

    Update Tween.lua

--- Tween.lua
+++ Tween.lua
@@ -1,6 +1,6 @@
 -- custom tween
 
-local Signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/vozoid/utility/main/Signal.lua"))()
+local Signal = loadstring(game:HttpGet("https://raw.githubusercontent.com/f-5ve/storage/refs/heads/main/Signal.lua"))()
 
 local render = game:GetService("RunService").RenderStepped
 local sqrt, sin, pi, halfpi, doublepi = math.sqrt, math.sin, math.pi, math.pi / 2, math.pi * 2
